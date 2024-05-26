4e9deb2d89ceee09e176b89ec481340452becd3e
vinunarwal <vinunarwal3@gmail.com>
2024-05-26 07:01:31 -0500

    ..

--- controller/waterController.js
+++ controller/waterController.js
@@ -1,4 +1,6 @@
 const { Contact } = require("../models/contactSchema");
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const addFields = async (req, res) => {
   try {
@@ -29,6 +31,30 @@ const addFields = async (req, res) => {
     });
 
     await newContact.save();
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', 
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS 
+      }
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER, 
+      subject: 'New Contact',
+      text: `New Contact  received:\n\nName: ${firstName} ${lastName}\nEmail: ${email}\n Number: ${number} \n 
+       Company Name : ${company} \n Business Type : ${businessType} \n Advertising : ${advertising} \n Budget : ${budget} \n Message: ${message} `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
 
     res.status(201).json({
       message: "Contact added successfully",
